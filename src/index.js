94ed78f2cfcb15e18c0fc05ccd861bb502cd3173
IvanKapitula <kapitula.ivan@yandex.ru>
2023-01-11 18:16:50 +0300

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,75 @@
 module.exports = function check(str, bracketsConfig) {
+  let arr = bracketsConfig.map(function(item){
+    return item.join('');
+  }
+  );
+  let strArray = str.split('');
+  for(let k = 0; k < arr.length;k++){
+    for(let i = 0; i < strArray.length;i++){
+      if(strArray.join('').indexOf(arr[k]) !== -1){
+        strArray.splice(strArray.join('').indexOf(arr[k]),2)
+      }
+    }
+  }
+
+  if(strArray.join('') !== ''){
+    for(let k = 0; k < arr.length;k++){
+      for(let i = 0; i < strArray.length;i++){
+        if(strArray.join('').indexOf(arr[k]) !== -1){
+          strArray.splice(strArray.join('').indexOf(arr[k]),2)
+        }
+      }
+    }
+  }
+  if(strArray.join('') !== ''){
+    for(let k = 0; k < arr.length;k++){
+      for(let i = 0; i < strArray.length;i++){
+        if(strArray.join('').indexOf(arr[k]) !== -1){
+          strArray.splice(strArray.join('').indexOf(arr[k]),2)
+        }
+      }
+    }
+  }
+  if(strArray.join('') !== ''){
+    for(let k = 0; k < arr.length;k++){
+      for(let i = 0; i < strArray.length;i++){
+        if(strArray.join('').indexOf(arr[k]) !== -1){
+          strArray.splice(strArray.join('').indexOf(arr[k]),2)
+        }
+      }
+    }
+  }
+  if(strArray.join('') !== ''){
+    for(let k = 0; k < arr.length;k++){
+      for(let i = 0; i < strArray.length;i++){
+        if(strArray.join('').indexOf(arr[k]) !== -1){
+          strArray.splice(strArray.join('').indexOf(arr[k]),2)
+        }
+      }
+    }
+  }
+  if(strArray.join('') !== ''){
+    for(let k = 0; k < arr.length;k++){
+      for(let i = 0; i < strArray.length;i++){
+        if(strArray.join('').indexOf(arr[k]) !== -1){
+          strArray.splice(strArray.join('').indexOf(arr[k]),2)
+        }
+      }
+    }
+  }
+  if(strArray.join('') !== ''){
+    for(let k = 0; k < arr.length;k++){
+      for(let i = 0; i < strArray.length;i++){
+        if(strArray.join('').indexOf(arr[k]) !== -1){
+          strArray.splice(strArray.join('').indexOf(arr[k]),2)
+        }
+      }
+    }
+  }
+  if(strArray.join('') === ''){
+    return true;
+  } else {
+return false;
+  }
   // your solution
 }
